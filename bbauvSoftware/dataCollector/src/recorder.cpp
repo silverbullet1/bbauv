417562464ae043ecec5fc97f1443dff5fa30ad4e
searcher <searcher@searcher-K53SV>
2012-12-18 11:52:04 +0800

    push launch file, dataCollector

---
+++ bbauvSoftware/dataCollector/src/recorder.cpp
@@ -0,0 +1,49 @@
+#include <ros/ros.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <boost/foreach.hpp>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <bbauv_msgs/compass_data.h>
+#include <bbauv_msgs/sensors_data.h>
+#include <std_msgs/String.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+string bagfile_path = "bag_files/2012-12-17-12-51-44.bag";
+char textfile_path[] = "text_files/testing.txt";
+
+int main(int argc, char** argv) {
+	ros::init(argc,argv,"test");
+	ros::NodeHandle n;
+	rosbag::Bag bag;
+	bag.open(bagfile_path, rosbag::bagmode::Read);
+	vector<string> topics;
+	topics.push_back(string("os5000_data"));
+	rosbag::View view(bag, rosbag::TopicQuery(topics));
+	
+	ofstream myfile;
+	myfile.open(textfile_path);
+	
+
+	BOOST_FOREACH(rosbag::MessageInstance const m, view) {
+		std_msgs::Header::ConstPtr h=m.instantiate<std_msgs::Header>();
+		if (h!=NULL) {
+			myfile << h->stamp << ", ";
+		}
+		bbauv_msgs::compass_data::ConstPtr s=m.instantiate<bbauv_msgs::compass_data>();
+		if (s!=NULL) {
+			myfile << s->yaw << ", ";
+			myfile << s->pitch << ", ";
+			myfile << s->roll << ", ";
+			myfile << s->temperature << ", ";
+			myfile << s->Ax << ", ";
+			myfile << s->Ay << ", ";
+			myfile << s->Az << endl;
+		}
+	}
+	bag.close();
+	return 0;
+}
--- bbauvSoftware/sensors/sensors/src/sensorsSubscriber.cpp
+++ bbauvSoftware/sensors/sensors/src/sensorsSubscriber.cpp
@@ -1,24 +1,75 @@
 #include <ros/ros.h>
-#include <std_msgs/String.h>
+#include <bbauv_msgs/env_data.h>
 #include <bbauv_msgs/compass_data.h>
+#include <bbauv_msgs/sensors_data.h>
 using namespace std;
 
+float yaw,pitch,roll,temperature,Ax,Ay,Az;
+float Temp0,Temp1,Temp2,Depth,WaterDetA,WaterDetB,WaterDetC;
+
 void compassCallback(const bbauv_msgs::compass_data::ConstPtr& compassMsg) {
-	ROS_INFO("Heading: %f\n", compassMsg->yaw);
-	ROS_INFO("pitch: %f\n", compassMsg->pitch);
-	ROS_INFO("roll: %f\n", compassMsg->roll);
-	ROS_INFO("temperature: %f\n", compassMsg->temperature);
-	ROS_INFO("Ax: %f\n",compassMsg->Ax);
-	ROS_INFO("Ay: %f\n",compassMsg->Ay);
-	ROS_INFO("Az: %f\n",compassMsg->Az);
+	yaw = compassMsg->yaw;
+	pitch = compassMsg->pitch;
+	roll = compassMsg->roll;
+	temperature = compassMsg->temperature;
+	Ax = compassMsg->Ax;
+	Ay = compassMsg->Ay;
+	Az = compassMsg->Az;	
+	ROS_INFO("yaw        : %f\n",yaw);
+	ROS_INFO("pitch      : %f\n",pitch);
+	ROS_INFO("roll       : %f\n",roll);
+	ROS_INFO("temperature: %f\n",temperature);
+	ROS_INFO("Ax         : %f\n",Ax);
+	ROS_INFO("Ay         : %f\n",Ay);
+	ROS_INFO("Az         : %f\n",Az);
+}
+
+void envCallBack(const bbauv_msgs::env_data::ConstPtr& envMsg) {
+	Temp0 = envMsg->Temp0;
+	Temp1 = envMsg->Temp1;
+	Temp2 = envMsg->Temp2;
+	Depth = envMsg->Depth;
+	WaterDetA = envMsg->WaterDetA;
+	WaterDetB = envMsg->WaterDetB;
+	WaterDetC = envMsg->WaterDetC;
+	ROS_INFO("Temp0      : %f\n",Temp0);
+	ROS_INFO("Temp1      : %f\n",Temp1);
+	ROS_INFO("Temp2      : %f\n",Temp2);
+	ROS_INFO("Depth      : %f\n",Depth);
+	ROS_INFO("Water Det A: %f\n",WaterDetA);
+	ROS_INFO("Water Det B: %f\n",WaterDetB);
+	ROS_INFO("Water Det C: %f\n",WaterDetC);
 }
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "sensorsSubscriber");
-	ros::NodeHandle n;
+	ros::NodeHandle nh;
 
-	ros::Subscriber sub = n.subscribe("os5000_data", 1000, compassCallback);
+	ros::Subscriber compassSub = nh.subscribe("os5000_data", 1000, compassCallback);
+	ros::Subscriber envSub = nh.subscribe("env_data", 1000, envCallBack);
+	ros::Publisher pub = nh.advertise<bbauv_msgs::sensors_data>("sensors_logic",1000);
+	bbauv_msgs::sensors_data pubdata;
+	ros::Rate loop_rate(10);
 
-	ros::spin();
+	while (nh.ok()) {
+		pubdata.yaw = yaw;
+		pubdata.pitch = pitch;
+		pubdata.roll = roll;
+		pubdata.temperature = temperature;
+		pubdata.Ax = Ax;
+		pubdata.Ay = Ay;
+		pubdata.Az = Az;
+		pubdata.Temp0 = Temp0;
+		pubdata.Temp1 = Temp1;
+		pubdata.Temp2 = Temp2;
+		pubdata.Depth = Depth;
+		pubdata.WaterDetA = WaterDetA;
+		pubdata.WaterDetB = WaterDetB;
+		pubdata.WaterDetC = WaterDetC;
+		pub.publish(pubdata);
+		
+		ros::spin();
+		loop_rate.sleep();
+	}
 	return 0;
 }
