774fa0b72df4f694f9fe9655955a055943778895
thanhquang <thanhquang@ubuntu>
2012-12-19 21:30:11 +0800

    update arduino control code

--- bbauvSoftware/arduino_codes/motorControllerSubscriberPololu2/motorControllerSubscriberPololu2.ino
+++ bbauvSoftware/arduino_codes/motorControllerSubscriberPololu2/motorControllerSubscriberPololu2.ino
@@ -8,8 +8,14 @@
 #define txPin2 8 
 #define rxPin2 9
 
-#define txPin3 17 
-#define rxPin3 16
+#define txPin3 33 
+#define rxPin3 32
+
+#define txPin4 17 
+#define rxPin4 16
+
+#define txPin5 37 
+#define rxPin5 36
 
 ros::NodeHandle nh;
 
@@ -20,21 +26,21 @@ ros::Subscriber<bbauv_msgs::thruster> control_sub("motor_controller",&motorContr
 SoftwareSerial smcSerial1 = SoftwareSerial(rxPin1, txPin1);
 SoftwareSerial smcSerial2 = SoftwareSerial(rxPin2, txPin2);
 SoftwareSerial smcSerial3 = SoftwareSerial(rxPin3, txPin3);
-//SoftwareSerial smcSerial4 = SoftwareSerial(rxPin4, txPin4);
-//SoftwareSerial smcSerial5 = SoftwareSerial(rxPin5, txPin5);
+SoftwareSerial smcSerial4 = SoftwareSerial(rxPin4, txPin4);
+SoftwareSerial smcSerial5 = SoftwareSerial(rxPin5, txPin5);
 
 // required to allow motors to move
 // must be called when controller restarts and after any error
 
-SoftwareSerial getSmc (uint8_t motor_index)
+SoftwareSerial getSmc(uint8_t motor_index)
 {
   switch(motor_index)
   {
     case 1: return smcSerial1;
     case 2: return smcSerial2;
     case 3: return smcSerial3;
-//    case 4: return smcSerial4;
-//    case 5: return smcSerial5;
+    case 4: return smcSerial4;
+    case 5: return smcSerial5;
   }
 }
 
@@ -67,8 +73,8 @@ void setup()
   smcSerial1.begin(19200);
   smcSerial2.begin(19200);
   smcSerial3.begin(19200);
-//  smcSerial4.begin(19200);
-//  smcSerial5.begin(19200);
+  smcSerial4.begin(19200);
+  smcSerial5.begin(19200);
   
   // the Simple Motor Controller must be running for at least 1 ms
   // before we try to send serial data, so we delay here for 5 ms
@@ -80,16 +86,17 @@ void setup()
   smcSerial1.write(0xAA);  // send baud-indicator byte
   smcSerial2.write(0xAA);
   smcSerial3.write(0xAA);
-//  smcSerial4.write(0xAA);
-//  smcSerial5.write(0xAA);
+  smcSerial4.write(0xAA);
+  smcSerial5.write(0xAA);
   
   // next we need to send the Exit Safe Start command, which
   // clears the safe-start violation and lets the motor run
   exitSafeStart(1);  // clear the safe-start violation and let the motor run
   exitSafeStart(2);
   exitSafeStart(3);
-//  exitSafeStart(4);
-//  exitSafeStart(5);
+  exitSafeStart(4);
+  exitSafeStart(5);
+
   nh.initNode();
   nh.subscribe(control_sub);
 }
@@ -106,5 +113,6 @@ void motorControl(const bbauv_msgs::thruster &msg)
   setMotorSpeed(1,msg.speed1);
   setMotorSpeed(2,msg.speed2);
   setMotorSpeed(3,msg.speed3);
-  
+  setMotorSpeed(4,msg.speed4);
+  setMotorSpeed(5,msg.speed5);
 }
