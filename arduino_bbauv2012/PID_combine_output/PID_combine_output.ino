22333152f306dbb98250cbc0702a0dc6b43fed80
Louis <trthanhquang@gmail.com>
2013-01-31 18:06:48 +0800

    add Forward/Backward/Sidemove PIDs. update aggregator and bbauv_msgs to be suitable with 5 PIDs

---
+++ arduino_bbauv2012/PID_combine_output/PID_combine_output.ino
@@ -0,0 +1,250 @@
+//using for 3rd integration
+#include <smcDriver.h>
+#include <PID_v1.h>
+#include <controller_input.h>
+#include <controller_param.h>
+#include <thruster.h>
+#include <ros.h>
+#include <SoftwareSerial.h>
+
+//ROS initialize
+ros::NodeHandle nh;
+
+int manual_speed[6];
+void teleopControl(const bbauv_msgs::thruster &msg);
+ros::Subscriber<bbauv_msgs::thruster> teleopcontrol_sub("teleop_controller",&teleopControl);
+
+void updateControllerParam(const bbauv_msgs::controller_param &msg);
+ros::Subscriber<bbauv_msgs::controller_param> pidconst_sub("controller_config",&updateControllerParam);
+
+void updateControllerInput(const bbauv_msgs::controller_input &msg);
+ros::Subscriber<bbauv_msgs::controller_input> controller_sub("controller_input",&updateControllerInput);
+
+bbauv_msgs::thruster thrusterSpeed;
+ros::Publisher thruster_pub("thruster_feedback",&thrusterSpeed);
+
+
+//smcDriver initialize
+#define rxPin 36 // Orange wire <-- receive from the 1st SMC Tx pin
+#define txPin 37 // Red wire --> transmit to all SMCs Rx pin
+smcDriver mDriver= smcDriver(rxPin,txPin);
+
+//PID initialize
+bool inDepthPID, inHeadingPID, inForwardPID, inBackwardPID, inSidemovePID, resetPID;
+
+double depth_setpoint,depth_input,depth_output;
+PID depthPID(&depth_input, &depth_output, &depth_setpoint,1,0,0, DIRECT);
+
+double heading_setpoint,heading_input,heading_output;
+PID headingPID(&heading_input, &heading_output, &heading_setpoint,1,0,0, DIRECT);
+
+double forward_setpoint,forward_input,forward_output;
+PID forwardPID(&forward_input, &forward_output, &forward_setpoint,1,0,0, DIRECT);
+
+double backward_setpoint,backward_input,backward_output;
+PID backwardPID(&backward_input, &backward_output, &backward_setpoint,1,0,0, DIRECT);
+
+double sidemove_setpoint,sidemove_input,sidemove_output;
+PID sidemovePID(&sidemove_input, &sidemove_output, &sidemove_setpoint,1,0,0, DIRECT);
+
+void setup()
+{
+  //initialize value for variables
+   inDepthPID= false;
+  inHeadingPID= false;
+  inForwardPID=false;
+  inBackwardPID=false;
+  inSidemovePID=false;
+  
+  resetPID=true;
+  
+  for(int i=0;i<6;i++)
+    manual_speed[i]=0;
+    
+  //initialize Motor driver and ROS
+  mDriver.init();
+  nh.initNode();
+  nh.subscribe(controller_sub);
+  nh.subscribe(pidconst_sub);
+  nh.subscribe(teleopcontrol_sub);
+  nh.advertise(thruster_pub);
+  
+  //setup depth PIDs
+  depthPID.SetMode(AUTOMATIC);
+  depthPID.SetSampleTime(20);
+  depthPID.SetOutputLimits(-2560,2560);
+  depthPID.SetControllerDirection(REVERSE);
+  
+  headingPID.SetMode(AUTOMATIC);
+  headingPID.SetSampleTime(20);
+  headingPID.SetOutputLimits(-1280,1280);
+  headingPID.SetControllerDirection(DIRECT);
+  
+  forwardPID.SetMode(AUTOMATIC);
+  forwardPID.SetSampleTime(20);
+  forwardPID.SetOutputLimits(0,2560);
+  forwardPID.SetControllerDirection(DIRECT);
+  
+  backwardPID.SetMode(AUTOMATIC);
+  backwardPID.SetSampleTime(20);
+  backwardPID.SetOutputLimits(0,2560);
+  backwardPID.SetControllerDirection(DIRECT);
+  
+  sidemovePID.SetMode(AUTOMATIC);
+  sidemovePID.SetSampleTime(20);
+  sidemovePID.SetOutputLimits(-2560,2560);
+  sidemovePID.SetControllerDirection(DIRECT);
+  
+  pinMode(13, OUTPUT); 
+}
+
+void runThruster()
+{
+  mDriver.setMotorSpeed(1,thrusterSpeed.speed1);
+  mDriver.setMotorSpeed(2,thrusterSpeed.speed2);
+  mDriver.setMotorSpeed(3,thrusterSpeed.speed3);
+  mDriver.setMotorSpeed(4,thrusterSpeed.speed4);
+  mDriver.setMotorSpeed(5,thrusterSpeed.speed5);
+  mDriver.setMotorSpeed(6,thrusterSpeed.speed6);  
+}
+
+void getTeleopControllerUpdate()
+{
+  thrusterSpeed.speed1=manual_speed[0];
+  thrusterSpeed.speed2=manual_speed[1];
+  thrusterSpeed.speed3=manual_speed[2];
+  thrusterSpeed.speed4=manual_speed[3];   
+  thrusterSpeed.speed5=manual_speed[4];
+  thrusterSpeed.speed6=manual_speed[5];    
+}
+
+void getDepthPIDUpdate()
+{
+  depthPID.SetMode(inDepthPID);
+  if(inDepthPID)
+  {
+    depthPID.Compute();
+  }
+}
+
+void getHeadingPIDUpdate()
+{
+  headingPID.SetMode(inHeadingPID);
+  if(inHeadingPID)
+  {
+    headingPID.Compute();
+  }
+}
+
+void getForwardPIDUpdate()
+{
+  forwardPID.SetMode(inForwardPID);
+  if(inForwardPID)
+  {
+    forwardPID.Compute();
+  }
+}
+
+void getBackwardPIDUpdate()
+{
+  backwardPID.SetMode(inBackwardPID);
+  if(inBackwardPID)
+  {
+    backwardPID.Compute();
+  }
+}
+
+void getSidemovePIDUpdate()
+{
+  sidemovePID.SetMode(inSidemovePID);
+  if(inSidemovePID)
+  {
+    sidemovePID.Compute();
+  }
+}
+
+void calculateThrusterSpeed()
+{
+  getTeleopControllerUpdate();
+  getDepthPIDUpdate();
+  getForwardPIDUpdate();
+  getBackwardPIDUpdate();
+  getHeadingPIDUpdate();
+  getSidemovePIDUpdate();
+  
+  thrusterSpeed.speed1=heading_output-forward_output+backward_output;
+  thrusterSpeed.speed2=-heading_output-forward_output+backward_output;
+  thrusterSpeed.speed3=heading_output+forward_output-backward_output;
+  thrusterSpeed.speed4=-heading_output+forward_output-backward_output;
+  thrusterSpeed.speed5=depth_output;
+  thrusterSpeed.speed6=depth_output;
+
+}
+
+void loop()
+{
+  //calculate final M.Speed for each thruster
+  calculateThrusterSpeed();
+  
+  //execute the calculated thruster speed
+  runThruster();
+  
+  //publish thruster speed info
+  thruster_pub.publish(&thrusterSpeed);
+  
+  nh.spinOnce();
+  delay(20);
+}    
+
+
+void updateControllerParam(const bbauv_msgs::controller_param &msg)
+{
+  float ratio[6]={msg.ratio_t1, msg.ratio_t2, msg.ratio_t3, 
+                           msg.ratio_t4, msg.ratio_t5,msg.ratio_t6};
+  mDriver.setThrusterRatio(ratio);
+  
+  depthPID.SetTunings(msg.depth_kp,msg.depth_ki,msg.depth_kd);
+  inDepthPID= msg.depth_PID;
+  
+  headingPID.SetTunings(msg.heading_kp,msg.heading_ki,msg.heading_kd);
+  inHeadingPID= msg.heading_PID;
+  
+  forwardPID.SetTunings(msg.forward_kp,msg.forward_ki,msg.forward_kd);
+  inForwardPID= msg.forward_PID;
+
+  backwardPID.SetTunings(msg.backward_kp,msg.backward_ki,msg.backward_kd);
+  inBackwardPID= msg.backward_PID;
+
+  sidemovePID.SetTunings(msg.sidemove_kp,msg.sidemove_ki,msg.sidemove_kd);
+  inSidemovePID= msg.sidemove_PID;
+  
+  resetPID=msg.reset;
+}
+
+void updateControllerInput(const bbauv_msgs::controller_input &msg)
+{
+  depth_input=msg.depth_input;
+  depth_setpoint=msg.depth_setpoint;
+  
+  heading_input=msg.heading_input;
+  heading_setpoint=msg.heading_setpoint;
+  
+  forward_input=msg.forward_input;
+  forward_setpoint=msg.forward_setpoint;
+  
+  backward_input=msg.backward_input;
+  backward_setpoint=msg.backward_setpoint;
+  
+  sidemove_input=msg.sidemove_input;
+  sidemove_setpoint=msg.sidemove_setpoint;
+}
+
+void teleopControl(const bbauv_msgs::thruster &msg)
+{
+  manual_speed[0]=msg.speed1;
+  manual_speed[1]=msg.speed2;
+  manual_speed[2]=msg.speed3;
+  manual_speed[3]=msg.speed4;
+  manual_speed[4]=msg.speed5;
+  manual_speed[5]=msg.speed6;
+}
--- ros_bbauv2012/Controller/aggregator/src/aggregator.cpp
+++ ros_bbauv2012/Controller/aggregator/src/aggregator.cpp
@@ -15,6 +15,9 @@ void update_setpoint(const bbauv_msgs::controller_setpoint sp);
 void collect_depth(const bbauv_msgs::env_data& msg);
 void collect_heading(const bbauv_msgs::compass_data& msg);
 
+//for forward, backward, sidemove. Remember to change msg type & edit the function below
+void collect_velocity(const bbauv_msgs::env_data& msg);
+
 void dynamic_reconfigure_callback(aggregator::controller_paramConfig &config, uint32_t level); 
 
 bbauv_msgs::controller_input ctrl;
@@ -22,17 +25,24 @@ bbauv_msgs::controller_param param;
 
 ros::Publisher controller_param_pub;
 ros::Publisher controller_input_pub;
-ros::Subscriber depth_sub; 
-ros::Subscriber compass_sub; 
+
 ros::Subscriber controller_setpoint_sub; 
+ros::Subscriber depth_sub; 
+ros::Subscriber compass_sub;
+//name can be updated
+ros::Subscriber velocity_sub; 
+
+
 
 int main(int argc,char** argv) {
   ros::init(argc,argv,"aggregator");
   ros::NodeHandle nh;
   //subscribers declaration
+  controller_setpoint_sub = nh.subscribe("controller_setpoint",20,update_setpoint,ros::TransportHints().tcpNoDelay());
   depth_sub = nh.subscribe("env_data",20,collect_depth,ros::TransportHints().tcpNoDelay());
   compass_sub = nh.subscribe("os5000_data",20,collect_heading,ros::TransportHints().tcpNoDelay());
-  controller_setpoint_sub = nh.subscribe("controller_setpoint",20,update_setpoint,ros::TransportHints().tcpNoDelay());
+  //topic name, msg type need to be updated
+  velocity_sub = nh.subscribe("vel_data",20,collect_velocity,ros::TransportHints().tcpNoDelay());
   
   //publishers declaration
   controller_input_pub = nh.advertise<bbauv_msgs::controller_input>("controller_input",20);
@@ -59,6 +69,9 @@ void update_setpoint(const bbauv_msgs::controller_setpoint sp)
 {
   ctrl.depth_setpoint=sp.depth_setpoint;
   ctrl.heading_setpoint=sp.heading_setpoint;
+  ctrl.forward_setpoint=sp.forward_setpoint;
+  ctrl.backward_setpoint=sp.backward_setpoint;
+  ctrl.sidemove_setpoint=sp.sidemove_setpoint;
 }
 
 void collect_depth(const bbauv_msgs::env_data& msg)
@@ -70,6 +83,15 @@ void collect_heading(const bbauv_msgs::compass_data& msg)
   ctrl.heading_input=msg.yaw;
 }
 
+//this is what we are doing when we receive the velocity data
+//the goal is to find out the input to send to PID
+//ctrl.forward_input, ctrl.backward_input, ctrl.sidemove
+void collect_velocity(const bbauv_msgs::env_data& msg)
+{
+  //...
+}
+
+
 void dynamic_reconfigure_callback(aggregator::controller_paramConfig &config, uint32_t level) 
 {
   param.depth_kp=config.depth_kp;	
@@ -79,15 +101,32 @@ void dynamic_reconfigure_callback(aggregator::controller_paramConfig &config, ui
   param.heading_kp=config.heading_kp;
   param.heading_ki=config.heading_ki;
   param.heading_kd=config.heading_kd;
-		
+
+  param.forward_kp=config.forward_kp;
+  param.forward_ki=config.forward_ki;
+  param.forward_kd=config.forward_kd;
+
+  param.backward_kp=config.backward_kp;
+  param.backward_ki=config.backward_ki;
+  param.backward_kd=config.backward_kd;
+
+  param.sidemove_kp=config.sidemove_kp;
+  param.sidemove_ki=config.sidemove_ki;
+  param.sidemove_kd=config.sidemove_kd;
+
   param.ratio_t1=config.ratio_t1;	
   param.ratio_t2=config.ratio_t2;	
   param.ratio_t3=config.ratio_t3;	
   param.ratio_t4=config.ratio_t4;	
   param.ratio_t5=config.ratio_t5;	
   param.ratio_t6=config.ratio_t6;	
+
   param.depth_PID=config.depth_PID;
   param.heading_PID=config.heading_PID;
+  param.forward_PID=config.forward_PID;
+  param.backward_PID=config.backward_PID;
+  param.sidemove_PID=config.sidemove_PID;
+
   param.reset=config.reset;
 }
 
