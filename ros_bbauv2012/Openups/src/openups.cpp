1db79f5047ba2ae87eb4a50a4430ae399ebaf397
Jon Ong <devnull@localhost>
2013-02-02 07:55:15 +0800

    Cleaned up OpenUPS code a bit.

--- ros_bbauv2012/Openups/src/openups.cpp
+++ ros_bbauv2012/Openups/src/openups.cpp
@@ -7,6 +7,7 @@
 #include <ros/ros.h>
 #include <bbauv_msgs/openups.h>
 
+const int NUM_UPS = 4;
 
 using namespace std;
 
@@ -27,10 +28,15 @@ int main(int argc, char** argv)
 	ros::Publisher pub = n.advertise<bbauv_msgs::openups>("openups",1000);
 	ros::Rate loop_rate(10);
 
+	ostringstream os;
 	while (ros::ok())
 	{
-		{	system("upsc openups1@localhost > tmp1");
-			ifstream infile("tmp1");
+		for (int ups_id=1; ups_id<=NUM_UPS; ++ups_id)
+		{
+			os.clear();
+			os << "upsc openups" << ups_id << "@localhost > tmp";
+			system(os.str().c_str());
+			ifstream infile("tmp");
 
 			if (infile)
 			{
@@ -42,79 +48,14 @@ int main(int argc, char** argv)
 				openupsMsg.batteryRuntime=battery_runtime(s);
 				openupsMsg.batteryVoltage=battery_voltage(s);
 
-				ROS_INFO("openups1: %d %.3lfA %dsec %.2lfV\n", openupsMsg.batteryCharge, openupsMsg.batteryCurrent, openupsMsg.batteryRuntime, openupsMsg.batteryVoltage);
+				ROS_INFO("openups%d: %d %.3lfA %dsec %.2lfV\n", ups_id, openupsMsg.batteryCharge, openupsMsg.batteryCurrent, openupsMsg.batteryRuntime, openupsMsg.batteryVoltage);
 			}
 			pub.publish(openupsMsg);
 
-			ros::spinOnce();
-			loop_rate.sleep();
-
 		}
 
-		{	system("upsc openups2@localhost > tmp2");
-			ifstream infile("tmp2");
-
-			if (infile)
-			{
-				char s[10000];
-				infile.get(s, 1000, 0);
-
-				openupsMsg.batteryCharge=battery_charge(s);
-				openupsMsg.batteryCurrent=battery_current(s);
-				openupsMsg.batteryRuntime=battery_runtime(s);
-				openupsMsg.batteryVoltage=battery_voltage(s);
-
-				ROS_INFO("openups2: %d %.3lfA %dsec %.2lfV\n", openupsMsg.batteryCharge, openupsMsg.batteryCurrent, openupsMsg.batteryRuntime, openupsMsg.batteryVoltage);
-			}
-			pub.publish(openupsMsg);
-
-			ros::spinOnce();
-			loop_rate.sleep();
-
-		}
-		{	system("upsc openups3@localhost > tmp3");
-			ifstream infile("tmp3");
-
-			if (infile)
-			{
-				char s[10000];
-				infile.get(s, 10000, 0);
-
-				openupsMsg.batteryCharge=battery_charge(s);
-				openupsMsg.batteryCurrent=battery_current(s);
-				openupsMsg.batteryRuntime=battery_runtime(s);
-				openupsMsg.batteryVoltage=battery_voltage(s);
-
-				ROS_INFO("openups3: %d %.3lfA %dsec %.2lfV\n", openupsMsg.batteryCharge, openupsMsg.batteryCurrent, openupsMsg.batteryRuntime, openupsMsg.batteryVoltage);
-			}
-			pub.publish(openupsMsg);
-
-			ros::spinOnce();
-			loop_rate.sleep();
-
-		}
-		{	system("upsc openups4@localhost > tmp4");
-			ifstream infile("tmp4");
-
-			if (infile)
-			{
-				char s[10000];
-				infile.get(s, 10000, 0);
-
-				openupsMsg.batteryCharge=battery_charge(s);
-				openupsMsg.batteryCurrent=battery_current(s);
-				openupsMsg.batteryRuntime=battery_runtime(s);
-				openupsMsg.batteryVoltage=battery_voltage(s);
-
-				ROS_INFO("openups4: %d %.3lfA %dsec %.2lfV\n", openupsMsg.batteryCharge, openupsMsg.batteryCurrent, openupsMsg.batteryRuntime, openupsMsg.batteryVoltage);
-			}
-			pub.publish(openupsMsg);
-
-			ros::spinOnce();
-			loop_rate.sleep();
-
-		}
-
+		ros::spinOnce();
+		loop_rate.sleep();
 	}
 
 	return 0;
