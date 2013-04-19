9c456df9836d1abe32496f8a9991f177fc9e469e
ongtianchang <ongtianchang@thinkPadx230>
2013-04-19 15:05:38 +0800

    Parking Task with HSV segmentation

--- ros_bbauv2012/Logic/Vision/nodes/parking (3rd April copy).py
+++ ros_bbauv2012/Logic/Vision/nodes/parking (3rd April copy).py
@@ -2,15 +2,12 @@
 
 import roslib; roslib.load_manifest('Vision')
 import rospy
-
+from ros2opencv2 import ROS2OpenCV2
 import cv2
 from cv2 import cv as cv
-from cv_bridge import CvBridge, CvBridgeError
-
 from std_msgs.msg import String
-from sensor_msgs.msg import Image, RegionOfInterest
+from sensor_msgs.msg import Image
 import numpy as np
-import sys
 
 # State Machine
 import smach
@@ -32,66 +29,134 @@ class Standby():
     def __init__(self):
         print "standby"
         
-class Initialization:
-    def __init__(self):
-        self.node_name = "Parking"
-        rospy.init_node(self.node_name)
-        rospy.loginfo("Starting Node " + str(self.node_name))
+class Initialization(ROS2OpenCV2):
+    def __init__(self, node_name):
+        rospy.init_node(node_name)
+        rospy.loginfo("Starting Node " + str(node_name))
         print "init'ing'"
 
-class Parking_Proc:
-    def __init__(self):                
-        self.image_sub = rospy.Subscriber("input_rgb_image", Image, self.image_callback)    
-        self.bridge = CvBridge()
+class Parking_Proc(ROS2OpenCV2):
+    def __init__(self, node_name="Parking"):
+        ROS2OpenCV2.__init__(self, node_name)
         
-        self.hmin = rospy.get_param("~hmin", 70)
-        self.hmax = rospy.get_param("~hmax", 80)
-        self.smin = rospy.get_param("~smin", 80)
-        self.smax = rospy.get_param("~smax", 140)
-        self.vmin = rospy.get_param("~vmin", 150)
-        self.vmax = rospy.get_param("~vmax", 255)
-        self.hsv = {'hmin': self.hmin,'hmax':self.hmax,'smin':self.smin,'smax':self.smax,'vmin':self.vmin,'vmax':self.vmax,}        
-
+        self.debug_mode = rospy.get_param("~debug_mode",True)         
         self.a_maxval = rospy.get_param("~a_maxval",255)        
         self.a_method = rospy.get_param("~a_method",1)        
         self.a_threshtype = rospy.get_param("~a_threshtype",1)
         self.a_block_size = rospy.get_param("~a_blocksize",15)        
-        self.a_constant = rospy.get_param("~a_constant", 5)
-        self.threshold = rospy.get_param("~threshold", 0)   
-        self.debug_mode = rospy.get_param("~debug_mode", True)       
+        self.a_constant = rospy.get_param("~a_constant", 5)        
+                             
+        self.h_rho = rospy.get_param("~h_rho",2)
+        self.h_theta = rospy.get_param("~h_theta",90)
+        self.h_thresh = rospy.get_param("~h_thresh",100)        
+        self.h_minlinelength = rospy.get_param("~h_minlinelength",120)
         
+        self.smin = rospy.get_param("~smin", 0)
+        self.vmin = rospy.get_param("~vmin", 0)
+        self.vmax = rospy.get_param("~vmax", 254)
+        self.threshold = rospy.get_param("~threshold", 0)                
+         
         print "image processing "
-        
-    def image_callback(self, data):
-        frame = self.convert_image(data)
-        processed_image = self.process_image(frame)
 
-        
-    def convert_image(self, ros_image):
-        # Use cv_bridge() to convert the ROS image to OpenCV format
-        try:
-            cv_image = self.bridge.imgmsg_to_cv(ros_image, "bgr8")       
-            return np.array(cv_image, dtype=np.uint8)
-        except CvBridgeError, e:
-            print e
-            
     def process_image(self, frame):
-        
-        frame2 = frame3 = frame
-        
-        horizontal_green = self.hsv_thresholding(frame2, hmin=self.hmin, hmax=self.hmax, smin=self.smin, smax=self.smax, vmin=self.vmin, vmax=self.vmax)
 
-#        adapt = self.adaptive_thresholding(frame3)
-        
-        cam = self.cam_shift(frame3)
+# Call for Cam Shift
 
-#        if self.debug_mode:
-        cv2.imshow("HSV Thresholded", horizontal_green)
-#        cv2.imshow("Adaptive Thresholded", adapt)
-                            
-        cv.WaitKey(1)
+#        frame2 = frame
+        cam = Parking_Proc.cam_shift(self,frame2)
+#       
+# Call for Line Detection
+
+        line = Parking_Proc.close_adaptive(self, frame)
+
         return frame
 
+    def hsv_thresholding(self,frame, hmin=0, hmax=180, smin=0, smax=255, vmin=0, vmax=255):
+        
+        #Assume frame is BGR format
+        hsv_img = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+             
+        #initialize matrix for HSV min and max values
+        COLOR_MIN = np.array([hmin, smin, vmin],np.uint8)
+        COLOR_MAX = np.array([hmax, smax, vmax],np.uint8)
+        
+        #Execute Thresholding
+        hsv_threshed = cv2.inRange(hsv_img, COLOR_MIN, COLOR_MAX)
+        
+        if self.debug_mode:
+            cv2.imshow("HSV Thresholded", hsv_threshed)
+#            cv2.namedWindow("HSV", cv2.CV_WINDOW_AUTOSIZE)
+#            cv2.createTrackbar("Hue Min", "HSV", self.hsv['hmin'], 180,  self.paramSetter('hmin'))
+#            cv2.createTrackbar("Hue Max", "HSV", self.hsv['hmax'], 180,  self.paramSetter('hmax'))
+#            cv2.createTrackbar("Sat Min", "HSV", self.hsv['smin'], 255,  self.paramSetter('smin'))
+#            cv2.createTrackbar("Sat Max", "HSV", self.hsv['smax'], 255,  self.paramSetter('smax'))        
+#            cv2.createTrackbar("Val Min", "HSV", self.hsv['vmin'], 255,  self.paramSetter('smin'))
+#            cv2.createTrackbar("Val Max", "HSV", self.hsv['vmax'], 255,  self.paramSetter('smax'))    
+        return hsv_threshed
+
+    def paramSetter(self, key):
+        def setter(val):
+	        self.hsv[key] = val
+        return setter        
+
+    def close_adaptive(self, frame):
+#Gray Scale        
+
+        frame = cv2.GaussianBlur(frame, (3,3),2)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+##Morphological Operations        
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))
+        gray = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel, iterations=2)    
+        gray = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel,iterations=5)
+
+##Creating a copy to draw on        
+        overlayed = frame
+        
+#getting size of image        
+        m,n = gray.shape
+
+#Adaptive Thresholding
+        edges = cv2.adaptiveThreshold(gray,self.a_maxval,self.a_method,self.a_threshtype,self.a_block_size,self.a_constant)
+
+        if self.debug_mode:
+        
+            cv.NamedWindow("Gray", 0)
+            cv.MoveWindow("Gray", 0, 0)
+            cv2.imshow("Gray", gray)
+            
+            cv.NamedWindow("Edges", 0)
+            cv.MoveWindow("Edges", 330, 0)
+            cv2.imshow("Edges", edges)
+
+        return edges
+
+    def prob_hough(self, frame):
+
+        overlayed = frame
+#Probabilistic Line Detection
+
+        plines = cv2.HoughLinesP(frame, self.h_rho, np.pi/self.h_theta, self.h_thresh, np.array([]), self.h_minlinelength)[0]
+        print plines
+
+# Draw Red lines
+        for l in plines[:1]:
+            cv2.line(overlayed, (l[0],l[1]), (l[2],l[3]), (0,0,255), 2)
+                
+        x = (l[2]-l[0])+l[0]/2
+        y = (l[1]+l[3])/2
+            
+        print "x = %d & y = %d" % (x , y)
+
+# Debugging Windows
+
+        if self.debug_mode:
+        
+            cv.NamedWindow("Hough", 0)
+            cv.MoveWindow("Hough", 660, 0)
+            cv2.imshow("Hough", overlayed)        
+            
+    
     def cam_shift(self, frame):
         # First blue the image
         blur = cv2.blur(frame, (5, 5))
@@ -176,39 +241,24 @@ class Parking_Proc:
             histimg = cv2.cvtColor(histimg_hsv, cv.CV_HSV2BGR)
             
             return histimg
-	
-    def adaptive_thresholding(self, input_frame, maxval=255, method=1, threshtype=1, block_size=15, constant=5):
-
-        blur = cv2.GaussianBlur(input_frame, (3,3), 2)
-        gray = cv2.cvtColor(input_frame, cv2.COLOR_BGR2GRAY)
-        adaptive_threshed = cv2.adaptiveThreshold(gray, maxval, method, threshtype, block_size, constant)
-
-        return adaptive_threshed
-		
-    def hsv_thresholding(self, input_frame, hmin=0, hmax=180, smin=0, smax=255, vmin=0, vmax=255):
         
-        #Assume frame is BGR format
-        hsv_img = cv2.cvtColor(input_frame,cv2.COLOR_BGR2HSV)
-             
-        #initialize matrix for HSV min and max values
-        COLOR_MIN = np.array([hmin, smin, vmin],np.uint8)
-        COLOR_MAX = np.array([hmax, smax, vmax],np.uint8)
-        
-        #Execute Thresholding
-        hsv_threshed = cv2.inRange(hsv_img, COLOR_MIN, COLOR_MAX)
-        
-        return hsv_threshed
+            
+def main():
+    node_name = "Parking"
 
-def main(args):
+if __name__ == '__main__':
     try:
-        Initialization()
+        node_name = "Parking"
+        Initialization(node_name)
         Parking_Proc()
         Standby()
+        try:
+            rospy.init_node(node_name)
+        except:
+            pass
         rospy.spin()
     except KeyboardInterrupt:
         print "Shutting down vision node."
-        cv.DestroyAllWindows()   
+        cv.DestroyAllWindows()
 
-if __name__ == '__main__':
-    main(sys.argv)
 
