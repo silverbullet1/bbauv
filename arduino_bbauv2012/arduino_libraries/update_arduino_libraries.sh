59b45c9bd423383ce272718d8e70300ca98b33f6
Louis <trthanhquang@gmail.com>
2013-04-20 17:39:39 +0800

    smcDriver_v2 - bash script to update arduino libraries

--- arduino_bbauv2012/arduino_libraries/update_arduino_libraries.sh
+++ arduino_bbauv2012/arduino_libraries/update_arduino_libraries.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo cp -r smcDriver_v2 /usr/share/arduino/libraries/
+sudo cp -r PID_v1 /usr/share/arduino/libraries/
+
 rosrun rosserial_client make_library.py . bbauv_msgs
 
 sudo cp -r ros_lib/bbauv_msgs/ /usr/share/arduino/libraries/
