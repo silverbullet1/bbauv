213eec39fc196492dd2bca8d2a253edc503b884c
Jon Ong <jon@jon.games-ignition.com>
2013-05-20 12:55:01 +0800

    Updated battery notifier.

--- ros_bbauv2012/Topside/clan_cp/audible_notifiers/nodes/battery_notifier.py
+++ ros_bbauv2012/Topside/clan_cp/audible_notifiers/nodes/battery_notifier.py
@@ -15,23 +15,23 @@ def main():
 
     def callback(data):
 
-        charge_status['oUPS1'] = data.batteries[0].batteryCharge
-        charge_status['oUPS2'] = data.batteries[1].batteryCharge
-        charge_status['oUPS3'] = data.batteries[2].batteryCharge
-        charge_status['oUPS4'] = data.batteries[3].batteryCharge
-    
+        charge_status['oUPS1'] = data.battery1
+        charge_status['oUPS2'] = data.battery2
+        charge_status['oUPS3'] = data.battery3
+        charge_status['oUPS4'] = data.battery4
+
     rospy.init_node('battery_notifier', anonymous=True)
     rospy.Subscriber("openups", openups, callback)
 
     while not rospy.is_shutdown():
         
         for key,value in charge_status.iteritems():
-            if value == -1:
+            if value == 255:
                 canberra = pycanberra.Canberra()
                 canberra.easy_play_sync("suspend-error")
                 canberra.destroy()  
                 rospy.loginfo('Hey dude, give me some POWER man.')
-                        
+
         rospy.sleep(1)
         rospy.on_shutdown(destroyCanberra)
 
