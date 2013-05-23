7c431847cfa291407b73414ec9c4638d3e0ccac8
Jon Ong <jon@jon.games-ignition.com>
2013-05-23 10:43:25 +0800

    Updated lane marker detector to use Hough lines instead.

--- ros_bbauv2012/Logic/Vision/nodes/lane_marker_detector.py
+++ ros_bbauv2012/Logic/Vision/nodes/lane_marker_detector.py
@@ -10,13 +10,17 @@ from sensor_msgs.msg import Image
 from bbauv_msgs.msg import compass_data
 
 import math
+from random import randint
 import numpy as np
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 
 
+DEBUG = False
+
+
 # Helper function to normalize heading
-def normHeading(heading):
+def norm_heading(heading):
     if heading > 360:
         return heading - 360
     if heading < 0:
@@ -26,8 +30,8 @@ def normHeading(heading):
 # Helper function to parameterize a ray
 def get_ray(pt, angle):
     radians = angle / 180.0 * math.pi
-    u = -math.sin(radians)
-    v = math.cos(radians)
+    u = math.cos(radians)
+    v = math.sin(radians)
     return ( pt, (u,v) )
 
 # Helper function to get the t's of the intersection of the parameterized rays
@@ -76,7 +80,7 @@ class LaneDetector:
         self.heading = 0.0
 
         # Configurable parameters
-        self.params = { 'hueLow': 11, 'hueHigh': 35, 'contourMinArea': 15 }
+        self.params = { 'hueLow': 11, 'hueHigh': 65, 'contourMinArea': 15 }
 
         # Set up param configuration window
         def paramSetter(key):
@@ -88,14 +92,6 @@ class LaneDetector:
         cv2.createTrackbar("Hue High:", "settings", self.params['hueHigh'], 180, paramSetter('hueHigh'));
         cv2.createTrackbar("Min contour area (1/1000):", "settings", self.params['contourMinArea'], 1000, paramSetter('contourMinArea'));
 
-        ## Example filters
-        ## Original image - its Laplacian
-        #tmpkernel = np.array([[0., -1., 0.],[-1., 5., -1.], [0., -1., 0.]])
-        #sublaplacefilter = lambda (cv_img): cv2.filter2D(cv_img, -1, tmpkernel)
-
-        ## Canny (edge detection)
-        #cannyfilter = lambda (_): cv2.threshold(cv2.Canny(grayfilter.image, 125, 350), 128, 255, cv2.THRESH_BINARY_INV)[1]
-
 
     # Callback for subscribing to Image topic
     def gotRosFrame(self, rosImage):
@@ -104,12 +100,6 @@ class LaneDetector:
 
     # Function that gets called after conversion from ROS Image to OpenCV image
     def gotFrame(self, cvimg):
-        # Perform some processing
-
-        # Ignore all blue stuff
-#        channels = cv2.split(cvimg)
-#        imgNew = cv2.merge([np.zeros(channels[1].shape, channels[1].dtype), channels[1], channels[2]])
-
         # Find the orange regions
         imghsv = cv2.cvtColor(cvimg, cv2.cv.CV_BGR2HSV)
 #        imghsv = cv2.equalizeHist(imghsv)
@@ -117,89 +107,100 @@ class LaneDetector:
         imgBW = cv2.inRange(imghsv, np.array([self.params['hueLow'],0,0]), np.array([self.params['hueHigh'], 255, 255]))
 
         # Close up the gaps in the detected regions
-        structuringElt = cv2.getStructuringElement(cv2.MORPH_RECT,
-                                (3,3), (1,1))
+        structuringElt = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3), (1,1))
         imgBW = cv2.dilate(imgBW, structuringElt)
         imgBW = cv2.erode(imgBW, structuringElt)
 
         # Make a copy because findContours modifies the original image
         imgBW2 = imgBW.copy()
 
-        # Retrieve the contours of the black regions
+        # Retrieve the contours of the important regions
         contours, _ = cv2.findContours(imgBW2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        # Find the bounding rectangles of the contours that are larger than our threshold
-        # Give an array of rects in dictionaries of the form:
-        # {
-        #    'rect':   ((center_x,center_y), (w,h), theta),
-        #    'points': [... array of the 4 corners of the rect ...],
-        #    'edges':  [... array of 2 perpendicular edges (as vectors) of the rect ...],
-        #    'angle':  (float) angle of the box from the vertical (0 deg), with positive counterclockwise,
-        #    'heading':(float) heading that the box should be pointing to
-        # }
-        foundRects = [ ]
+        # Find the bounding rects of the contours that are larger than our threshold
+        areaThreshold = self.params['contourMinArea'] * cvimg.shape[0] * cvimg.shape[1] / 1000
+        contourRects = []
         for contour in contours:
             curArea = cv2.contourArea(contour)
-            if curArea >= self.params['contourMinArea'] * cvimg.shape[0] * cvimg.shape[1] / 1000:
-                maxRect = cv2.minAreaRect(contour)
-                foundRects.append({'rect': maxRect})
+            if curArea >= areaThreshold:
+                contourRects.append(cv2.minAreaRect(contour))
 
-        # Perform operations on rectangles found
-        count = 0
-        for rect in foundRects:
-            # Obtain the actual corners of the box
-            points = cv2.cv.BoxPoints(rect['rect'])
+        debugTmp, debugTmp2 = None, None
+        if DEBUG:
+            debugTmp = np.zeros_like(imgBW)
+            debugTmp2 = cv2.merge([debugTmp, debugTmp, debugTmp])
 
-            rect['points'] = points
-            rect['edges'] = edges = [ np.array(points[1]) - np.array(points[0]), np.array(points[2]) - np.array(points[1]) ]
+        foundLines = []
 
-            # Calculate angle from the vertical
-            if cv2.norm(edges[0]) > cv2.norm(edges[1]):
-                rect['angle'] = math.atan(edges[0][0] / edges[0][1]) / math.pi * 180
-            else:
-                rect['angle'] = math.atan(edges[1][0] / edges[1][1]) / math.pi * 180
+        # Find lines in each region
+        for rect in contourRects:
+            mask = np.zeros_like(imgBW)
+            points = np.int0(cv2.cv.BoxPoints(rect))
+            cv2.fillPoly(mask, [points], 255)
+            rectImg = np.bitwise_and(imgBW, mask)
 
-            if rect['angle'] == 90:
-                # Need some way to distinguish between left-pointing and right-pointing
-                # when the box is horizontal:
-                # we'll assume that if the centre of the box is on the right, it points to the right
-                if rect['rect'][0][0] > (cvimg.shape[1] / 2):
-                    rect['angle'] = -90
+            if DEBUG:
+                debugTmp = np.bitwise_or(rectImg, debugTmp)
 
-            # Try to correct the angles if we have more lines to test against
-            if count > 0:
-                # Construct rays from the centres of the boxes
-                ray1 = get_ray(foundRects[0]['rect'][0], foundRects[0]['angle'])
-                ray2 = get_ray(rect['rect'][0], rect['angle'])
-                (t1, t2) = ray_intersection(ray1, ray2)
-                # We want t1 and t2 to both be negative
-                # If t1 is positive, flip ray2; and vice versa
-                if t1 > 0:
-                    rect['angle'] += 180 if rect['angle'] < 0 else -180
-                if t2 > 0:
-                    foundRects[0]['angle'] += 180 if foundRects[0]['angle'] < 0 else -180
+            lines = cv2.HoughLinesP(rectImg, 1, cv2.cv.CV_PI/180, 80, 30, 10)
 
-            count += 1
+            # Find median gradient of lines
+            # Store as (x,y) (centre of box), angle (orientation of box)
+            gradient = 0
+            if lines != None:
+                gradients = [math.atan2(endy-starty, endx-startx)
+                                for startx, starty, endx, endy in lines[0]]
+                gradient = np.median(gradients)
 
-            # Draw the lines for debugging
-            imgBW = cv2.merge([imgBW, imgBW, imgBW])
-            for i in range(4):
-                # The line function doesn't accept floating point values
-                pt1 = (int(points[i][0]), int(points[i][1]))
-                pt2 = (int(points[(i+1)%4][0]), int(points[(i+1)%4][1]))
-                cv2.line(imgBW, pt1, pt2, (255,255,0), 2)
+                angle = np.rad2deg(gradient)
 
-        #TODO: publish the rects (with target headings)
+                if angle == 0:
+                    # Need some way to distinguish between left-pointing and right-pointing
+                    # when the line is horizontal:
+                    # we'll assume that if the centre of the box is on the right, it points to the right
+                    if rect[0][0] > (rectImg.shape[1] / 2):
+                        angle = 180
 
-        # Debug output
-        count = 0
-        for rect in foundRects:
-            print 'Angle', count, ':', rect['angle']
-            count += 1
+                # Try to correct the angles if we have more lines to test against
+                if len(foundLines) > 0:
+                    # Construct rays from the centres of the boxes
+                    ray1 = get_ray(foundLines[0]['pos'], foundLines[0]['angle'])
+                    ray2 = get_ray(rect[0], angle)
+                    (t1, t2) = ray_intersection(ray1, ray2)
+                    # We want t1 and t2 to both be negative
+                    # If t1 is positive, flip ray2; and vice versa
+                    if t2 > 0:
+                        angle += (180 if angle < 0 else -180)
+                    if t1 > 0:
+                        foundLines[0]['angle'] += (180 if foundLines[0]['angle'] < 0 else -180)
+
+                foundLines.append({'pos':rect[0], 'angle':angle})
+
+                if DEBUG:
+                    # Draw bounding rect
+                    for i in range(4):
+                        # The line function doesn't accept floating point values
+                        pt1 = (points[i][0], points[i][1])
+                        pt2 = (points[(i+1)%4][0], points[(i+1)%4][1])
+                        cv2.line(debugTmp2, pt1, pt2, (0,0,255), 1)
+
+        for line in foundLines:
+            line['heading'] = norm_heading(-line['angle'] - 90)
 
         # Screens for debugging
-#        cv2.imshow("src", cvimg)
-        cv2.imshow("hsv", imghsv)
-        cv2.imshow("bw", imgBW)
+        if DEBUG:
+            for i,line in enumerate(foundLines):
+                print i, line
+
+                # Draw a debug line
+                startpt = line['pos']
+                gradient = np.deg2rad(line['angle'])
+                endpt = (int(startpt[0] + 100 * math.cos(gradient)), int(startpt[1] + 100 * math.sin(gradient)))
+                startpt = (int(startpt[0]), int(startpt[1]))
+                cv2.line(debugTmp2, startpt, endpt, (255,0,0), 2)
+
+            imgDebug = np.bitwise_xor(cv2.merge([debugTmp, debugTmp, debugTmp]), debugTmp2)
+            cv2.imshow("hsv", imghsv)
+            cv2.imshow("bw", imgDebug)
 
 
     # Callback for subscribing to compass data
