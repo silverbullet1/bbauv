f4030a413078a5b7e90bc144ee2312360f875552
Bumblebee AUV <bumblebeeauv@gmail.com>
2013-06-22 08:43:00 +0800

    Commit Again

--- ros_bbauv2012/Scripts/child/ros_boot.sh
+++ ros_bbauv2012/Scripts/child/ros_boot.sh
@@ -11,14 +11,14 @@ tmux rename-window 'Scripts 2'
 tmux new-window -tbbauv:2
 tmux rename-window 'roscore'
 tmux send-keys 'roscore' C-m
-sleep 1
+sleep 3
 tmux new-window -tbbauv:3
 tmux rename-window 'launch'
 tmux send-keys 'pls' C-m
 sleep 2
 tmux new-window -tbbauv:4
 tmux rename-window 'boot'
-tmux send-keys 'rostopic pub -1 /lcd_commands Int16 -- 1
+tmux send-keys 'rostopic pub -1 /lcd_commands std_msgs/Int8 -- 1' C-m
 tmux set-option -s mouse-resize-pane on
 tmux set-option -s mouse-select-pane on
 tmux set-option -s pane-border-fg blue
