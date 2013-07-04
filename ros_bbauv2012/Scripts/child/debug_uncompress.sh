cca6389728b0f56edf52eb6e6a13af7e51510024
yashas <yashas@Yasha.S-X1>
2013-07-04 22:26:22 +0800

    forget debug_uncompress.sh

--- ros_bbauv2012/Scripts/child/debug_uncompress.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ROS_NAMESPACE=/debug/stereo_camera/left rosrun image_transport republish compressed in:=image_rect_color raw out:=image_rect_color_opt &
-ROS_NAMESPACE=/debug/stereo_camera/right rosrun image_transport republish compressed in:=image_rect_color raw out:=image_rect_color_opt &
-ROS_NAMESPACE=/debug/bottomcam/camera rosrun image_transport republish compressed in:=image_rect_color raw out:=image_rect_color_opt &
-
