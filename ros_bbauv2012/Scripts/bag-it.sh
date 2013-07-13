dc57ded1d7dc71c3f769a1669a230efe0ba1234c
Bumblebee AUV <bumblebeeauv@gmail.com>
2013-07-14 02:33:08 +0800

    removed scripts

--- ros_bbauv2012/Scripts/bag-it.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Bag everything from the compressed cameras
-cd /media/0052-4D54
-rosbag record -b 0 -o full-vid-feed --split --size=3076 /bottomcam/camera/camera_info /bottomcam/camera/image_raw/compressed{,/parameter_descriptions,/parameter_updates} /stereo_camera/{left,right}/camera_info /stereo_camera/{left,right}/image_raw/compressed{,/parameter_descriptions,/parameter_updates}
