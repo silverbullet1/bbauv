2fe5296bf33beafc8d13314bfd7d511fd403be95
Bumblebee AUV <bumblebeeauv@gmail.com>
2013-07-14 02:30:02 +0800

    commited from bbauv

--- ros_bbauv2012/Scripts/bag-it-autonomously.sh
+++ ros_bbauv2012/Scripts/bag-it-autonomously.sh
@@ -2,4 +2,7 @@
 
 # Bag everything from the compressed cameras
 cd /media/sda1/AutonomousRuns
-rosbag record -b 0 -o autonom-runclear /Vision/image_filter /bottomcam/camera/camera_info /bottomcam/camera/image_raw/compressed{,/parameter_descriptions,/parameter_updates} /stereo_camera/{left,right}/camera_info /stereo_camera/{left,right}/image_raw/compressed{,/parameter_descriptions,/parameter_updates} /depth /euler /AHRS8_data_e /AHRS8_data_q /LocomotionServer/feedback /controller_points /thruster_speed /euler /WH_DVL_data /earth_odom /cmd_vel /cmd_position /openups /hull_status /rosout
+
+rosbag record --split --size=10240 -b 0 -o big-autonom /hydrophone/time_diff /Vision/image_filter /bottomcam/camera/camera_info /bottomcam/camera/image_raw/compressed{,/parameter_descriptions,/parameter_updates} /stereo_camera/{left,right}/camera_info /stereo_camera/{left,right}/image_raw/compressed{,/parameter_descriptions,/parameter_updates} /depth /altitude /euler /AHRS8_data_e /AHRS8_data_q /LocomotionServer/feedback /controller_points /thruster_speed /WH_DVL_data /earth_odom /cmd_vel /cmd_position /openups_stats /hull_status /rosout & 
+
+rosrun mission_planner v4_mission_planner.py
--- ros_bbauv2012/Scripts/bag-it-small-autonomously.sh
+++ ros_bbauv2012/Scripts/bag-it-small-autonomously.sh
@@ -2,7 +2,8 @@
 
 # Bag everything from the compressed cameras
 cd /media/sda1/AutonomousRuns
-rosbag record -b 0 -o autonomous-small /Vision/image_filter /debug/stereo_camera/left/image_rect_color /debug/stereo_camera/right/image_rect_color /debug/bottomcam/camera/image_rect_color /depth /euler /AHRS8_data_e /AHRS8_data_q /LocomotionServer/feedback /controller_points /thruster_speed /WH_DVL_data /earth_odom /cmd_vel /cmd_position /openups /hull_status /rosout &
 
-rosrun mission_planner v3_mission_planner.py
+rosbag record --split --size=10240 -b 0 -o small-autonom /hydrophone/time_diff /Vision/image_filter/compressed /debug/stereo_camera/left/image_rect_color /debug/bottomcam/camera/image_rect_color /depth /altitude /euler /LocomotionServer/feedback /controller_points /WH_DVL_data /earth_odom /cmd_vel /cmd_position /openups_stats /hull_status /rosout &
+
+rosrun mission_planner v4_mission_planner.py
 
