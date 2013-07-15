f5f8290fe5e44b87f55fbef2cb56fe3652db43d4
ongtianchang <ongtianchang@thinkPadx230>
2013-07-15 14:15:17 -0700

    more edits to visualize marker

--- ros_bbauv2012/Logic/mission_planner/src/v4_mission_planner.py
+++ ros_bbauv2012/Logic/mission_planner/src/v4_mission_planner.py
@@ -724,15 +724,15 @@ if __name__ == '__main__':
 
     with sm_mission:
         
-        smach.StateMachine.add('COUNTDOWN', Countdown(0), transitions={'succeeded':'START'})
-        smach.StateMachine.add('START',Start(10,0.2,0),
-                                transitions={'succeeded':'MARKER1'})
+        smach.StateMachine.add('COUNTDOWN', Countdown(0), transitions={'succeeded':'MARKER1'})
+#         smach.StateMachine.add('START',Start(10,0.2,0),
+#                                 transitions={'succeeded':'MARKER1'})
         
-        smach.StateMachine.add('MARKER1', StoreGlobalCoord('mission_marker1'), transitions={'succeeded':'MOVE_LEFT'})
-        smach.StateMachine.add('MOVE_LEFT', GoToDistance(30, 2, 'sway'), transitions={'succeeded':'MARKER2'})
-        smach.StateMachine.add('MARKER2', StoreGlobalCoord('mission_marker2'), transitions={'succeeded':'MOVE_FWD'})
-        smach.StateMachine.add('MOVE_FWD', GoToDistance(30, 2,'fwd'), transitions={'succeeded':'MARKER3'})
-        smach.StateMachine.add('MARKER3', StoreGlobalCoord('mission_marker3'), transitions={'succeeded':'mission_complete'})
+        smach.StateMachine.add('MARKER1', StoreGlobalCoord('mission_marker1'), transitions={'succeeded':'mission_complete'})
+#         smach.StateMachine.add('MOVE_LEFT', GoToDistance(30, 2, 'sway'), transitions={'succeeded':'MARKER2'})
+#         smach.StateMachine.add('MARKER2', StoreGlobalCoord('mission_marker2'), transitions={'succeeded':'MOVE_FWD'})
+#         smach.StateMachine.add('MOVE_FWD', GoToDistance(30, 2,'fwd'), transitions={'succeeded':'MARKER3'})
+#         smach.StateMachine.add('MARKER3', StoreGlobalCoord('mission_marker3'), transitions={'succeeded':'mission_complete'})
 
 ### Mission Ends Here ###  
 
