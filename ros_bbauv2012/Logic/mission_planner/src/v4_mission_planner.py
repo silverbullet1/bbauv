e30304b6df30a95f1d8db5887d91fc660f906d4c
Bumblebee AUV <bumblebeeauv@gmail.com>
2013-07-20 14:45:40 +0800

    night time nav test; found out about strange auv_gui killing compass node while untethered bug

--- ros_bbauv2012/Logic/mission_planner/src/v4_mission_planner.py
+++ ros_bbauv2012/Logic/mission_planner/src/v4_mission_planner.py
@@ -1186,17 +1186,17 @@ if __name__ == '__main__':
     sm_mission = smach.StateMachine(outcomes=['mission_complete','mission_failed'])
 
     with sm_mission:
-        smach.StateMachine.add('COUNTDOWN', Countdown(0), transitions={'succeeded':'START'})
+        smach.StateMachine.add('COUNTDOWN', Countdown(15), transitions={'succeeded':'START'})
         smach.StateMachine.add('START',Start(5,0.5,0),transitions={'succeeded':'WYPT1'})    
 #         smach.StateMachine.add('TURN_TO_GATE', GoToHeading(10, 70), transitions={'succeeded':'GO_TO_GATE'})
 #         smach.StateMachine.add('GO_TO_GATE', GoToDistance(30, 6, 'fwd'), transitions={'succeeded':'SURFACE_VICTORIOUSLY'})
-        smach.StateMachine.add('WYPT1', Nav(30,30,30, 1,1, 0.5,0.5,90), transitions={'succeeded':'WYPT2', 'failed':'SURFACE_SADLY'})
-        smach.StateMachine.add('WYPT2', Nav(30,30,30, 2,4, 0.5,0.5,90), transitions={'succeeded':'WYPT3', 'failed':'SURFACE_SADLY'})
-        smach.StateMachine.add('WYPT3', Nav(30,30,30, 1,-1, 0.5,0.5,90), transitions={'succeeded':'WYPT4', 'failed':'SURFACE_SADLY'})
+        smach.StateMachine.add('WYPT1', Nav(30,30,30, 2.4,1.3, 0.5,0.5,90), transitions={'succeeded':'WYPT2', 'failed':'SURFACE_SADLY'})
+        smach.StateMachine.add('WYPT2', Nav(30,30,30, 4,2.8, 0.5,0.5,90), transitions={'succeeded':'WYPT3', 'failed':'SURFACE_SADLY'})
+        smach.StateMachine.add('WYPT3', Nav(30,30,30, 1.9,3.1, 0.5,0.5,90), transitions={'succeeded':'WYPT4', 'failed':'SURFACE_SADLY'})
         smach.StateMachine.add('WYPT4', Nav(30,30,30, 1,1, 0.5,0.5,90), transitions={'succeeded':'SURFACE_VICTORIOUSLY', 'failed':'SURFACE_SADLY'})        
         smach.StateMachine.add('SURFACE_SADLY', GoToDepth(20, 0.26), transitions={'succeeded':'mission_failed'})
-        smach.StateMachine.add('SURFACE_VICTORIOUSLY', GoToDepth(5, 0.26), transitions={'succeeded':'VICTORY_SPIN'})
-        smach.StateMachine.add('VICTORY_SPIN', GoToHeading(60, 0, relative=True), transitions={'succeeded':'mission_complete'})
+        smach.StateMachine.add('SURFACE_VICTORIOUSLY', GoToDepth(5, 0.26), transitions={'succeeded':'mission_complete'})
+#        smach.StateMachine.add('VICTORY_SPIN', GoToHeading(60, 0, relative=True), transitions={'succeeded':'mission_complete'})
 
 
 ### Mission Ends Here ###  
