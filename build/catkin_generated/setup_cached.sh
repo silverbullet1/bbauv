0da861ad6ce99cfacaf5607f4c6a4218519efd0a
jason <jason@ubuntu>
2013-10-22 16:42:51 +0800

    Initialization of ROS Catkin Workspace.

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PWD="/home/jason/eclipseWorkspace/SAUVC2014/build"
+export ROS_PACKAGE_PATH="/home/jason/eclipseWorkspace/SAUVC2014/src:/opt/ros/hydro/share:/opt/ros/hydro/stacks"
