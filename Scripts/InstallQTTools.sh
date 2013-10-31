3c3dfebace69b1ba9cd68f2bfc0274f96bc5c3b4
huixian <huixian@huixian>
2013-10-31 18:11:21 +0800

    Updated InstalQTTools.sh to cd .. before installing

--- Scripts/InstallQTTools.sh
+++ Scripts/InstallQTTools.sh
@@ -7,10 +7,13 @@ sudo apt-get update
 sudo apt-get install qttools5-dev
 sudo apt-get install qt5-default
 
+#Please install in the default home folder
+cd ..
 wget http://download.qt-project.org/official_releases/qt/5.1/5.1.1/qt-linux-opensource-5.1.1-x86_64-offline.run
 chmod +x qt-linux-opensource-5.1.1-x86_64-offline.run
 ./qt-linux-opensource-5.1.1-x86_64-offline.run
 
 
 #create symbolic link
-ln -s $HOME/Qt5.1.1/5.1.1/gcc_64/bin /usr/lib/x86_64-linux-gnu/qt5/
+sudo rm -r /usr/lib/x86_64-linux-gnu/qt5/bin
+sudo ln -s $HOME/Qt5.1.1/5.1.1/gcc_64/bin /usr/lib/x86_64-linux-gnu/qt5/
