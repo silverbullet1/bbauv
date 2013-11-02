d4b95b0f47ec96c834157994e278f21f002e47c9
huixian <huixian@huixian>
2013-11-02 11:20:09 +0800

    - Added OpenFile() function to open file in QFileDialog and run rosbag play - Updated README to make it more readable

--- src/GUI/src/auv_gui.cpp
+++ src/GUI/src/auv_gui.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QFileDialog>
 #include "auv_gui.h"
 #include "ros/ros.h"
 #include "std_msgs/String.h"
@@ -10,8 +11,20 @@ static void chatterCallback(const std_msgs::String::ConstPtr& msg)
 	ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
-static void openFile(int selectedIndex){
+static void selectBottomFilter(int selectedIndex){
 	ui.bottomfilter->setItemText(selectedIndex,"Hello");
+}
+
+static void openFile(){
+	//QFileDialog to open file
+	QString filename = QFileDialog::getOpenFileName(this, tr("Open bag file"), QDir::currentPath(), 
+		tr("Bag files (*.bag);; All files (*.*)")
+		0, QFileDialog::DontUseNativeDialog);
+	if (!filename.isNull()) { qDebug.toAscii(); }
+	//Try to run the bag file from a new terminal in rosrun
+	char command[500];
+	strcpy(command, "gnome-terminal -e 'bash -c " + "\"" + "rosbag play "  + filename + "; exec bash\"" + "'");
+	system(command);
 }
 
 int main(int argc, char **argv)
@@ -23,7 +36,8 @@ int main(int argc, char **argv)
 	QMainWindow *window = new QMainWindow;
 	ui.setupUi(window);
 	
-	QObject::connect(ui.bottomfilter, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged), openFile);
+	QObject::connect(ui.bottomfilter, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged), selectBottomFilter);
+	QObject::connect(ui.actionOpen, static_cast<void(QAction::*)(bool)>(&QAction::triggered), openFile);
 
 	window->show();
 
