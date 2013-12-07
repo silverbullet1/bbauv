ec626562f6d0ed8e45a52de72b13039663c2f327
searcher <searcher@searcher-K53SV>
2013-12-07 18:15:15 +0800

    Merge.

--- bbauvSoftware/actuators/thruster_control/src/thrusterController.cpp
+++ bbauvSoftware/actuators/thruster_control/src/thrusterController.cpp
@@ -1,17 +1,14 @@
 #include "ros/ros.h"
-#include "bbauv_msgs/thruster.h"
-#include "sensor_msgs/Joy.h"
+//#include "bbauv_msgs/thruster.h"
+//#include "sensor_msgs/Joy.h"
 
 //declare publish message
-arduino::motor_msg msg;
+//bbauv_msgs::thruster msg;
 
-void cal_speed(const sensor_msgs::Joy::ConstPtr& joy)
-{
-  msg.d3=0;
-  msg.speed3=joy->axes[1]*2500;
-
-  
-}
+//void cal_speed(const sensor_msgs::Joy::ConstPtr& joy)
+//{
+//  msg.speed3=joy->axes[1]*2500;
+//}
 
 int main(int argc, char **argv)
 {
@@ -20,14 +17,14 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   
   //declare variables:
-//  arduino::motor_msg msg;
+//  bbauv_msgs::thruster msg;
 
   //subscribe another topic for control mission
   //ros:Subscriber taks_sub = n.subscribe("task",1000,execute_task); 
-  ros::Subscriber joy_sub=n.subscribe<sensor_msgs::Joy>("joy",10,cal_speed);
+//  ros::Subscriber joy_sub=n.subscribe<sensor_msgs::Joy>("joy",10,cal_speed);
 
   //publish message data to [topic]motor_controller:
-  ros::Publisher motor_pub = n.advertise<arduino::motor_msg>("motor_controller", 1000);
+//  ros::Publisher motor_pub = n.advertise<bbauv_msgs::thruster>("motor_controller", 1000);
   ros::Rate loop_rate(10);
 
   while (ros::ok())
@@ -37,7 +34,7 @@ int main(int argc, char **argv)
     //msg.speed3=1000;
     //msg.d2=1;
     //msg.speed2=000;
-    motor_pub.publish(msg);
+//    motor_pub.publish(msg);
 
     ros::spinOnce();
     loop_rate.sleep();
