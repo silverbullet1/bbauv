5f68cc8c5167a31f032fa16b03ada7fe89d6c671
Jon Ong <jonongjs@gmail.com>
2013-12-07 18:09:50 +0800

    write controlPublisher to publish data to actuators (raw version)

---
+++ bbauvSoftware/controller/controlPublisher/src/publisher.cpp
@@ -0,0 +1,37 @@
+#include <ros/ros.h>
+#include <bbauv_msgs/manual_control.h>
+#include <bbauv_msgs/thruster.h>
+using namespace std;
+
+const double sqrt2 = 1.4142;
+
+int x,y,z,yaw;
+
+void monitorCallBack(const bbauv_msgs::manual_control::ConstPtr& msg) {
+	x = msg->x;
+	y = msg->y;
+	z = msg->z;
+	yaw = msg->yaw;
+}
+
+int main(int argc,char** argv) {
+	ros::init(argc,argv,"publisher");
+	ros::NodeHandle nh;
+	bbauv::thruster thrusterMsg;
+	ros::Publisher pub = nh.advertise<bbauv_msgs/thruster>("controller_actuator",1000);
+	ros::Subcriber sub = nh.subscribe("monitor_controller",1000,monitorCallBack);
+	ros::Rate loop_rate(10);
+
+	while (ros::ok()) {
+		thrusterMsg.speed1 = z;
+		thrusterMsg.speed4 = z;
+		thrusterMsg.speed2 = (y + yaw)/(-2);
+		thrusterMsg.speed3 = ((yaw - y)/sqrt2 - x) / 2;
+		thrusterMsg.speed5 = (- (yaw - y)/sqrt2 - x) / 2;
+		pub.publish(thrusterMsg);
+
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	return 0;
+}
