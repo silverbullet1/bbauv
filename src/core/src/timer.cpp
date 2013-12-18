25a1062db96c430406d65c287daf36c0d52559f1
Jason Poh <jason@ubuntu.(none)>
2013-12-19 00:48:39 +0800

    Moved messages to a common package. (same folder for action, srv, msg)

--- src/core/src/timer.cpp
+++ src/core/src/timer.cpp
@@ -1,14 +1,12 @@
 #include "ros/ros.h"
-#include "std_msgs/Duration.h"
-#include "std_msgs/Time.h"
-#include "core/TimeElapsed.h"
+#include "msgs/TimeElapsed.h"
 #include <sstream>
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "timer");
 	ros::NodeHandle node;
-	ros::Publisher timer_publisher = node.advertise<core::TimeElapsed>("timer", 10);
+	ros::Publisher timer_publisher = node.advertise<msgs::TimeElapsed>("timer", 10);
 	ros::Duration loopDuration(1);
 
 	ros::Time start_time = ros::Time::now();
@@ -17,7 +15,7 @@ int main(int argc, char **argv)
 	{
 		//Calculate the duration elapsed since this node started
 		ros::Duration duration = ros::Time::now() - start_time;
-		core::TimeElapsed timeElapsed;
+		msgs::TimeElapsed timeElapsed;
 		timeElapsed.secondsElapsed = duration;
 		//Publishes the calculated duration
 		timer_publisher.publish(timeElapsed);
