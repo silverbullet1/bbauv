ca597801c307ded704d88857a0e95a7f5c710c8a
Jon Ong <jonongjs@gmail.com>
2013-12-27 15:57:58 +0800

    Added a script to calibrate cameras, a launch file to start the camera, and some calibration files.

---
+++ Scripts/CameraCalibration.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Grid setup of the internal points of the chessboard
+INTERNAL_PTS=9x6
+# Width of each square (in metres)
+SQUARE_WIDTH=0.024
+
+rosrun camera_calibration cameracalibrator.py --size $INTERNAL_PTS --square $SQUARE_WIDTH image:=/camera/image_raw camera:=/camera
