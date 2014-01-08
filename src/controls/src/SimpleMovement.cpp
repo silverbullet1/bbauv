bbe6bba5832b2aef470395a406a145ca5f63c824
Jason Poh <jason@ubuntu.(none)>
2014-01-08 22:33:25 +0800

    Added logging to SimpleMovement node

--- src/controls/src/SimpleMovement.cpp
+++ src/controls/src/SimpleMovement.cpp
@@ -6,46 +6,46 @@
 */
 
 #include "ros/ros.h"
-#include "bbauv_msgs/TimeElapsed.h"
+//#include "bbauv_msgs/TimeElapsed.h"
 
 bool doYaw(int val)
 {
-
+	ROS_INFO("SimpleMovement: Changing yaw by %d", val);
 }
 
 bool doRoll(int val)
 {
-
+	ROS_INFO("SimpleMovement: Changing roll by %d", val);
 }
 
 bool doPitch(int val)
 {
-
+	ROS_INFO("SimpleMovement: Changing pitch by %d", val);
 }
 
 bool goToDepth(int metres)
 {
-
+	ROS_INFO("SimpleMovement: Going to %d (m) deep", metres);
 }
 
 bool moveLeft(int metres)
 {
-
+	ROS_INFO("SimpleMovement: Moving left by %d metres", metres);
 }
 
 bool moveRight(int metres)
 {
-
+	ROS_INFO("SimpleMovement: Moving right by %d metres", metres);
 }
 
 bool rotateRight(int degrees)
 {
-
+	ROS_INFO("SimpleMovement: Rotating right by %d degrees", degrees);
 }
 
 bool rotateLeft(int degrees)
 {
-
+	ROS_INFO("SimpleMovement: Rotating left by %d degrees", degrees);
 }
 
 int main(int argc, char **argv)
