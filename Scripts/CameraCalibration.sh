054babeed993b167ee30374cc02ee6391b46bd56
ndt93 <thiennguyen1993@gmail.com>
2014-01-25 00:26:32 +0800

    Fix CameraCalibration.sh

--- Scripts/CameraCalibration.sh
+++ Scripts/CameraCalibration.sh
@@ -5,4 +5,4 @@ INTERNAL_PTS=9x6
 # Width of each square (in metres)
 SQUARE_WIDTH=0.024
 
-rosrun camera_calibration cameracalibrator.py --size $INTERNAL_PTS --square $SQUARE_WIDTH image:=/camera/image_raw camera:=/camera
+rosrun camera_calibration cameracalibrator.py --size $INTERNAL_PTS --square $SQUARE_WIDTH image:=bot_camera/camera/image_raw camera:=/bot_camera/camera
