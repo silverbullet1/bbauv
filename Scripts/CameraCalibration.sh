2d993ec336cf8a414835f71a03a4b1a7520ab932
bbauvsbc1 <bbauvsbc1@bbauv.(none)>
2014-01-25 00:30:42 +0800

    updated launch files and scripts

--- Scripts/CameraCalibration.sh
+++ Scripts/CameraCalibration.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Grid setup of the internal points of the chessboard
-INTERNAL_PTS=9x6
+INTERNAL_PTS=8x6
 # Width of each square (in metres)
 SQUARE_WIDTH=0.024
 
-rosrun camera_calibration cameracalibrator.py --size $INTERNAL_PTS --square $SQUARE_WIDTH image:=/camera/image_raw camera:=/camera
+rosrun camera_calibration cameracalibrator.py --size $INTERNAL_PTS --square $SQUARE_WIDTH image:=/bot_camera/camera/image_raw camera:=/bot_camera/camera
