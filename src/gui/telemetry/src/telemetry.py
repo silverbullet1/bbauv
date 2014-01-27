c283e6fb8c3184a91af3b40f481d2f18b931119d
jasonpoh <jason@codeit.sg>
2014-01-27 15:44:43 +0800

    Updated to 8 thrusters.

--- src/gui/telemetry/src/telemetry.py
+++ src/gui/telemetry/src/telemetry.py
@@ -520,10 +520,12 @@ class AUV_gui(QMainWindow):
         
         self.saPanel1.setText("<b>THR1: " + str(self.data['thrusters'].speed1) + 
                               "<br> THR2: " + str(self.data['thrusters'].speed2) +
-                              "<br> THR3: " + str(self.data['thrusters'].speed3) + "</b>")
-        self.saPanel2.setText("<b>THR4: " + str(self.data['thrusters'].speed4) +
-                              "<br> THR5: " + str(self.data['thrusters'].speed5) +
-                              "<br> THR6: " + str(self.data['thrusters'].speed6) + "</b>")
+                              "<br> THR3: " + str(self.data['thrusters'].speed3) +
+                              "<br> THR4: " + str(self.data['thrusters'].speed4) + "</b>")
+        self.saPanel2.setText("<b>THR5: " + str(self.data['thrusters'].speed5) +
+                              "<br> THR6: " + str(self.data['thrusters'].speed6) +
+                              "<br> THR7: " + str(self.data['thrusters'].speed7) + 
+                              "<br> THR8" + str(self.data['thrusters'].speed8) +  "</b>")
         
         mani_name = ["","","","","","",""]
         if self.data['manipulators'].servo1:
