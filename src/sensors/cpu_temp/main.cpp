e86c5b2a3218d8e11276d762136510cc51cec45e
spaghetti- <dev@xrng.net>
2014-02-20 21:09:16 +0800

    lol git because lazy to scp
    log for cpu temperature
    
    rename vriable
    
    this is dumb
    
    bugfix; temperature logger completed

--- src/sensors/cpu_temp/main.cpp
+++ src/sensors/cpu_temp/main.cpp
@@ -6,14 +6,13 @@
 
 #define NUMBER_OF_SENSORS 7 //socket and core +1
 
-void get_temperature(bbauv_msgs::cpu_temperature *ct, double *data)
+void get_temperature(bbauv_msgs::cpu_temperature *ct, double *data, FILE *logf)
 {
     const sensors_chip_name *cn;
     int c = 0, s = 0, f = 0, counter = 0;
     double val;
     //double data[NUMBER_OF_SENSORS];
 
-    sensors_init(NULL);
     while((cn = sensors_get_detected_chips(0, &c)) != 0)
     {
         const sensors_feature *features;
@@ -36,26 +35,35 @@ void get_temperature(bbauv_msgs::cpu_temperature *ct, double *data)
     double core_ave = (data[2] + data[2] + data[3]) / 3.0;
     ct->cores_ave = core_ave;
     ct->socket_ave = acpi_ave;
-    sensors_cleanup();
+    fprintf(logf, "socket_average: %lf\ncore_ave: %lf\n-----\n", acpi_ave, core_ave);
+    fflush(logf);
 }
 
 int main(int argc, char **argv)
 {
     const std::string cpu_temp_topic = "CPU_TEMP";
     double data[NUMBER_OF_SENSORS];
-    bbauv_msgs::cpu_temperature cpu_temperature; 
+    bbauv_msgs::cpu_temperature cpu_temperature;
     ros::init(argc, argv, "cpu_temperature");
+
+    FILE *logf  = fopen("/home/bbauvsbc1/cpu_temperature_log.txt", "w");
 
     ros::NodeHandle nodehandler;
     ros::Publisher publisher = nodehandler.advertise<bbauv_msgs::cpu_temperature>(cpu_temp_topic, 1000);
     ros::Rate loop_rate(0.2);
 
     //get_temperature(&cpu_temperature, data);
+    sensors_init(NULL);
 
     while(ros::ok()){
-        get_temperature(&cpu_temperature, data);
+        get_temperature(&cpu_temperature, data, logf);
         publisher.publish(cpu_temperature);
         loop_rate.sleep();
         ros::spinOnce();
     }
+
+    fclose(logf);
+    sensors_cleanup();
+
+    return 0;
 }
