33a5972a2998361e1778862ba6760c527ac1b48f
ndt93 <thiennguyen1993@gmail.com>
2014-03-03 19:39:25 +0800

    Some more changes

--- src/vision/scripts/bucket_task.py
+++ src/vision/scripts/bucket_task.py
@@ -109,7 +109,7 @@ class Centering(smach.State):
         deltaY = (rectData['centroid'][1] - screenCenterY) / screenHeight
         rospy.loginfo("x-off: %lf, y-off: %lf", deltaX, deltaY)
         
-        if abs(deltaX) < 0.01 and abs(deltaY) < 0.01:
+        if abs(deltaX) < 0.02 and abs(deltaY) < 0.02:
             self.bucketDetector.stopRobot()
             return 'centering_complete'
 
@@ -127,7 +127,7 @@ class Firing(smach.State):
     def execute(self, userdata):
         self.bucketDetector.depth_setpoint = 0.5
         self.bucketDetector.stopRobot()
-        self.bucketDetector.sendMovement(f=-0.1)
+        self.bucketDetector.sendMovement(f=-0.05)
         rospy.sleep(rospy.Duration(3.5))
 
         firePub = rospy.Publisher("/manipulators", manipulator)
