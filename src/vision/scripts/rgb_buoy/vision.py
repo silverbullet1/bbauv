ab878802159d30e6dd4b2a76d4bf54776004d555
huixian <lynnetteng2002@gmail.com>
2014-05-03 17:34:32 +0800

    Edited green vision rgb

--- src/vision/scripts/rgb_buoy/vision.py
+++ src/vision/scripts/rgb_buoy/vision.py
@@ -1,3 +1,5 @@
+#/usr/bin/env/python
+
 import math
 import numpy as np
 import cv2
@@ -5,14 +7,78 @@ import cv2
 from utils.utils import Utils
 
 class RgbBuoyVision:
-    #BLAH
-    pass
+    screen = {'width': 640, 'height': 480}
+    
+    #Vision parameters
+    greenThres = {'lo': (30, 0, 27), 'hi': (90, 147, 255)}
+    redThres = {'lo': (110, 0, 0), 'hi': (137, 255, 255)}
+    blueThres = {'lo': (100, 150, 0), 'hi': (140, 255, 255)}
+    
+    minContourArea = 5000
+    
+    rgbCount = {'red': 0, 'green': 0, 'yellow': 0}
+    
+    def __init__(self, debugMode = True):
+        self.debugMode = debugMode
+        
+    def gotFrame(self, img):        
+        #Set up parameters 
+        self.resetRGBCount()
+        foundLines = []
+        centroid = [0, 0]
+        outImg = None
+        rectData = {'foundLines': foundLines, 'centroid': centroid}
+        
+        #Preprocessing
+        img = cv2.resize(img, (self.screen['width'], self.screen['height']))
+        hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        hsvImg = cv2.GaussianBlur(hsvImg, ksize=(3, 3), sigmaX = 0)
+                
+        #Find red image
+        #redImg = self.threshold(img, "RED")
+        
+        #Find blue image
+        #blueImg = self.threshold(img, "BLUE")
+        
+        #Find green image 
+        greenImg = self.threshold(hsvImg, "GREEN")
+        outImg = greenImg
+        
+        #If any count is 3, bump
+        
+        #Combine images 
+        
+        return outImg
+    
+    def resetRGBCount(self):
+        self.rgbCount['red'] = 0
+        self.rgbCount['green'] = 0
+        self.rgbCount['yellow'] = 0
+    
+    def threshold(self, image, colour):
+        loThres, hiThres = self.getThreshVal(colour) 
+        binImg = cv2.inRange(image, loThres, hiThres)
+
+        thresImg = binImg 
+        return thresImg
+    
+    def getThreshVal(self, inColour):
+        colours = ["RED", "GREEN", "BLUE"]
+        
+        if inColour == colours[0]:
+            return self.redThres['lo'], self.redThres['hi']
+        elif inColour == colours[1]:
+            return self.greenThres['lo'], self.greenThres['hi']
+        else:
+            return self.blueThres['lo'], self.blueThres['hi']
 
 def main():
     cv2.namedWindow("test")
-    inImg = cv2.imread("rgb_buoy/test1.jpg")
+    
+    inImg = cv2.imread("rgb_buoy/RGB6.jpg")
     inImg = cv2.cvtColor(inImg, cv2.COLOR_RGB2BGR)
     detector = RgbBuoyVision()
-    _, outImg = detector.gotFrame(inImg)
+    outImg = detector.gotFrame(inImg)
+    
     if outImg is not None: cv2.imshow("test", outImg)
     cv2.waitKey()
--- src/vision/scripts/utils/utils.py
+++ src/vision/scripts/utils/utils.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env/python
+
 import rospy
 from cv_bridge import CvBridge, CvBridgeError
+from docutils.math.latex2mathml import math
 
 class Utils():
     bridge = CvBridge()
@@ -34,3 +37,23 @@ class Utils():
     def invertAngle(angle):
         if angle < 0: return angle + 180
         else: return angle - 180
+        
+    #Convert line equation to vector equation
+    @staticmethod
+    def vectorizeLine(pt, angle):
+        rad = angle / 180.0 * math.pi
+        u = math.cos(rad)
+        v = math.sin(rad)
+        return (pt, (u, v))
+    
+    #Find line intersections from line vector equations
+    @staticmethod
+    def findIntersection(line1, line2):
+        ((x1, y1), (u1, v1)) = line1
+        ((x2, y2), (u2, v2)) = line2
+        
+        det = 1.0 / (u2 * v1 - u1 * v2)
+        dx, dy = x2 - x1, y2 - y1
+        t1 = det * (-v2 * dx + u2 *dy)
+        
+        return (x1 + t1*u1, y1 + t1*v1)
