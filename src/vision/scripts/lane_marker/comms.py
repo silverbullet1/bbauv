74f95bfa3ca5b79ac5613a0a48df3e1b5ed244e9
ndt93 <thiennguyen1993@gmail.com>
2014-05-06 12:33:19 +0800

    Add more stuffs and fix bugs for lane_marker and utils. Move botcam generic communication to a separate module. Codes tested and working so far

--- src/vision/scripts/lane_marker/comms.py
+++ src/vision/scripts/lane_marker/comms.py
@@ -1,44 +1,18 @@
 import rospy
-from sensor_msgs.msg import Image
 
-from bbauv_msgs.msg import compass_data
-
-from utils.utils import Utils
-import utils.config as config
 from vision import LaneMarkerVision
+from bot_common.bot_comms import GenericComms
 
-class Comms:
+class Comms(GenericComms):
     """ Class to facilitate communication b/w ROS and task submodules """
 
-    inputHeading = 0
-    curHeading = 0
-
     def __init__(self):
-        # Initialize flags
-        self.canPublish = False
+        GenericComms.__init__(self, LaneMarkerVision())
 
-        #Initialize vision filter
-        self.visionFilter = LaneMarkerVision(self)
+        #TODO: Communicate with mission planner
 
-        # Get private params
-        self.isAlone = rospy.get_param('~alone', True)
-        self.imageTopic = rospy.get_param('~image', config.botCamTopic)
+    def handleSrv(self, data):
+        pass
 
-    def register(self):
-        self.camSub = rospy.Subscriber(self.imageTopic, Image, self.camCallback)
-        self.compassSub = rospy.Subscriber(config.compassTopic,
-                                           compass_data,
-                                           self.compassCallback)
-        self.outPub = rospy.Publisher(config.visionFilterTopic, Image)
-
-    def unregister(self):
-        self.camSub.unregister()
-        self.compassSub.unregister()
-
-    def camCallback(self, rosImg):
-        outImg = self.visionFilter.gotFrame(Utils.rosimg2cv(rosImg))
-        if self.canPublish:
-            self.outPub.publish(Utils.cv2rosimg(outImg))
-
-    def compassCallback(self, data):
-        self.curHeading = data.yaw
+def main():
+    testCom = Comms()
