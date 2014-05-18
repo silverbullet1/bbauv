80dffccfb653691621dcfe7e114cfbbf0aee1ada
ndt93 <thiennguyen1993@gmail.com>
2014-05-18 18:32:52 +0800

    Complete first version for bins state machine

--- src/vision/scripts/bins/states.py
+++ src/vision/scripts/bins/states.py
@@ -2,6 +2,7 @@ import rospy
 
 import smach, smach_ros
 
+from utils.utils import Utils
 from comms import Comms
 from vision import BinsVision
 
@@ -44,6 +45,8 @@ class Center(smach.State):
 
     width = BinsVision.screen['width']
     height = BinsVision.screen['height']
+    centerX = width / 2.0
+    centerY = height / 2.0
 
     xcoeff = 2.0
     ycoeff = 1.5
@@ -63,10 +66,46 @@ class Center(smach.State):
            len(self.comms.rectVal['matches']) == 0:
             return 'lost'
 
-        return 'aligning'
+        matches = self.comms.retVal['matches']
+        closestCentroid = None
+        closestDist = 1000
+        for match in matches:
+            dist = Utils.distBetweenPoints(match['centroid'],
+                                           (self.centerX, self.centerY))
+            if dist < closestDist:
+                closestCentroid = match['centroid']
+                closestDist = dist
+
+        dx = (closestCentroid[0] - self.centerX) / self.width
+        dy = (closestCentroid[1] - self.centerY) / self.height
+
+        if abs(dx) < self.maxdx and abs(dy) < self.maxdy:
+            self.comms.sendMovement(f=-self.ycoeff*dy, sm=self.xcoeff*dx,
+                                    blocking=False)
+            return 'centering'
+
+        self.comms.sendMovement(f=0.0, sm=0.0)
+        return 'centered'
+
+class Fire(smach.State):
+    def __init__(self, comms):
+        smach.State.__init__(self, outcomes=['completed',
+                                             'aborted'])
+        self.comms = comms
+
+    def execute(self, userdata):
+        if self.comms.isAborted or self.comms.isKilled:
+            return 'aborted'
+
+        self.comms.openDropper()
+        rospy.sleep(rospy.Duration(0.5))
+        self.comms.closeDropper()
+
+        return 'completed'
+
 
 def main():
-    rospy.init_node('pickup_node')
+    rospy.init_node('bins_node')
     myCom = Comms()
 
     sm = smach.StateMachine(outcomes=['succeeded', 'aborted', 'killed'])
@@ -82,9 +121,13 @@ def main():
                                             'aborted':'DISENGAGE'})
         smach.StateMachine.add('CENTER',
                                Center(myCom),
-                               transitions={'centered':'CENTER',
+                               transitions={'centered':'FIRE',
                                             'centering':'CENTER',
                                             'lost':'SEARCH',
+                                            'aborted':'DISENGAGE'})
+        smach.StateMachine.add('FIRE',
+                               Fire(myCom),
+                               transitions={'completed':'succeeded',
                                             'aborted':'DISENGAGE'})
 
     introServer = smach_ros.IntrospectionServer('mission_server',
