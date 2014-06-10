a3cc4a768033bf27cd8b1b735e4de953ba41d5f9
ndt93 <thiennguyen1993@gmail.com>
2014-06-11 00:45:10 +0800

    Implement GrayWorld and MaxRGB algorithm - tested

--- src/vision/scripts/bot_common/vision.py
+++ src/vision/scripts/bot_common/vision.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import math
 
+
 class Vision():
     SHAPE_TRI = 0
     SHAPE_RECT = 1
@@ -13,12 +14,13 @@ class Vision():
 
     _cosBounds = {'rect': (-0.1, 0.3), 'penta': (-0.34, -0.27),
                   'hexa': (-0.55, -0.45)}
+
     @staticmethod
     def drawRect(img, pts):
         points = np.int32(pts)
         for i in range(4):
             pt1 = (points[i][0], points[i][1])
-            pt2 = (points[(i+1)%4][0], points[(i+1)%4][1])
+            pt2 = (points[(i+1) % 4][0], points[(i+1) % 4][1])
             cv2.line(img, pt1, pt2, (0, 0, 255), 2)
 
     @staticmethod
@@ -70,16 +72,16 @@ class Vision():
             # Find the cosine for angles of the polygon
             cos = list()
             for i in range(2, vtc+1):
-                cos.append(Vision.angle(approx[i%vtc][0],
+                cos.append(Vision.angle(approx[i % vtc][0],
                                         approx[i-2][0],
-                                        approx[i-1][0])) 
+                                        approx[i-1][0]))
             # Sort the angles cosine from smallest to largest
             sorted(cos)
 
             # Get the smallest and largest angle cosine
             mincos = cos[0]
             maxcos = cos[-1]
-            
+
             if vtc == 4 and \
                mincos >= Vision._cosBounds['rect'][0] and \
                maxcos <= Vision._cosBounds['rect'][1]:
@@ -91,8 +93,8 @@ class Vision():
                     maxcos <= Vision._cosBounds['penta'][1]:
                 return Vision.SHAPE_PENTA
             elif vtc == 6 and \
-               mincos >= Vision._cosBounds['hexa'][0] and \
-               maxcos <= Vision._cosBounds['hexa'][1]:
+                    mincos >= Vision._cosBounds['hexa'][0] and \
+                    maxcos <= Vision._cosBounds['hexa'][1]:
                 return Vision.SHAPE_HEXA
         else:
             area = cv2.contourArea(contour)
@@ -104,6 +106,44 @@ class Vision():
                 return Vision.SHAPE_CIR
 
         return None
+
+    @staticmethod
+    def shadesOfGray(img):
+        """ Implementation of the Shades of Gray algorithm, which is a
+        combination of Gray World algorithm and MaxRGB algorithm, which are
+        used to normalize color images to achieve color constancy """
+
+        inB, inG, inR = cv2.split(img)
+        avgR = np.mean(inR)
+        avgG = np.mean(inG)
+        avgB = np.mean(inB)
+        avgGray = np.mean((avgB, avgG, avgR))
+
+        if avgB == 0:
+            outB = inB
+        else:
+            outB = (avgGray/avgB)*inB
+
+        if avgG == 0:
+            outG = inG
+        else:
+            outG = (avgGray/avgG)*inG
+
+        if avgR == 0:
+            outR = inR
+        else:
+            outR = (avgGray/avgR)*inR
+
+        maxRGB = (np.max(outR), np.max(outG), np.max(outB))
+        factor = np.max(maxRGB)
+        if factor > 1:
+            outR = 255*outR/factor
+            outG = 255*outG/factor
+            outB = 255*outB/factor
+
+        outImg = cv2.merge((np.uint8(outB), np.uint8(outG), np.uint8(outR)))
+        return outImg
+
 
 def main():
     img = cv2.imread("bot_common/shapes.png")
